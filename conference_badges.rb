869681fa6115262ee38f554f7b9fed85180541a6
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-08 22:12:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(people)
   end
 end
 
-def assign_rooms
+def assign_rooms(names)
   count=1
   people.each do |person|
      puts "Hello, #{person}! You'll be assigned to room #{count}!"
